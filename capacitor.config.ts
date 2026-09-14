6a71a4db4288258db94c7f1cfc286c6d987b848b
CrisporoCarlJanxent <cjcrisporo18@gmail.com>
2026-09-14 18:50:18 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Lost_And_Found',
+  webDir: 'dist'
+};
+
+export default config;
